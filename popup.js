a80976f240e1542fd101f592ea2938bc1800db4b
muhammed sihan <78970761+cyph1re@users.noreply.github.com>
2023-10-19 14:27:04 +0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -6,10 +6,34 @@ document.getElementById("replace-button").addEventListener("click", function() {
         // Get the current tab's URL
         var url = currentTab.url;
 
-        // Replace the keyword "example" with "shorts"
+        // Check if the URL already contains "watch?v=" or is not a YouTube URL
+        if (url.includes("watch?v=") || !isYouTubeURL(url)) {
+            // URL already modified or not a YouTube URL, do nothing
+            return;
+        }
+
+        // Replace the keyword "shorts/" with "watch?v="
         var newUrl = url.replace("shorts/", "watch?v=");
 
-        // Update the current tab's URL
-        chrome.tabs.create({ url: newUrl });
+        // Create a new tab (t2)
+        chrome.tabs.create({ url: newUrl }, function(newTab) {
+            // Add a delay before pausing the video in the current tab (t1)
+            setTimeout(function() {
+                // Execute a script in the current tab (t1) to pause the video
+                chrome.scripting.executeScript({
+                    target: { tabId: currentTab.id },
+                    function: function() {
+                        var video = document.querySelector("video");
+                        if (video) {
+                            video.pause();
+                        }
+                    },
+                });
+            }, 3000); // 3000 milliseconds (3 seconds) delay before pausing the video in t1
+        });
     });
 });
+
+function isYouTubeURL(url) {
+    return /^https:\/\/www\.youtube\.com\//.test(url);
+}
