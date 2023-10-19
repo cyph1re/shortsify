536ca0b789fd71e74967606f88e5d82c4da0d3c7
muhammed sihan <78970761+cyph1re@users.noreply.github.com>
2023-10-19 16:17:09 +0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,21 +1,22 @@
-document.getElementById("replace-button").addEventListener("click", function() {
-    // Get the current tab
+function handleReplaceButtonClick() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         var currentTab = tabs[0];
 
         // Get the current tab's URL
         var url = currentTab.url;
 
-        // Check if the URL already contains "watch?v=" or is not a YouTube URL
-        if (url.includes("watch?v=") || !isYouTubeURL(url)) {
-            // URL already modified or not a YouTube URL, do nothing
+        if (!isYouTubeURL(url)) {
+            alert("This is not a YouTube URL.");
             return;
         }
-
-        // Replace the keyword "shorts/" with "watch?v="
+        if (url.includes("watch?v=")) {
+            alert("This video is already a video.");
+            return;
+ 
+        // Check if the URL already contains "shorts/" and replace it with "watch?v="
         var newUrl = url.replace("shorts/", "watch?v=");
 
-        // Create a new tab (t2)
+        // Create a new tab with the modified URL
         chrome.tabs.create({ url: newUrl }, function(newTab) {
             // Add a delay before pausing the video in the current tab (t1)
             setTimeout(function() {
@@ -32,8 +33,17 @@ document.getElementById("replace-button").addEventListener("click", function() {
             }, 3000); // 3000 milliseconds (3 seconds) delay before pausing the video in t1
         });
     });
-});
+}
 
 function isYouTubeURL(url) {
     return /^https:\/\/www\.youtube\.com\//.test(url);
 }
+
+// Add a click event listener to the "replace-button" element when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    var replaceButton = document.getElementById("replace-button");
+
+    if (replaceButton) {
+        replaceButton.addEventListener("click", handleReplaceButtonClick);
+    }
+});
